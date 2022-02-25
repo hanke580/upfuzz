1d02982cef34fa5a3136fb0bd18dae9313397067
Yongle Zhang <yongle@pal-10-184-80-56.itap.purdue.edu>
2022-02-25 17:26:09 -0500

    added a simple test

---
+++ src/test/java/CommandTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.zlab.upfuzz.Command;
+import org.zlab.upfuzz.cassandra.CassandraCommands;
+import org.zlab.upfuzz.cassandra.CassandraState;
+
+public class CommandTests {
+
+    @Test
+    public void testCreateCommandGeneration() {
+        CassandraState s = new CassandraState();
+        Command cmd = new CassandraCommands.CREATETABLE(s);
+    }
+}
