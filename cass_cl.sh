5a6943b5ad36942646b2c1eae38f627591ecb31f
Ke Han <kehan5800@gmail.com>
2022-11-29 10:40:58 -0500

    update config

--- cass_cl.sh
+++ cass_cl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pgrep -f config.json | xargs sudo kill -9
-docker rm -f $(docker ps -a -q -f ancestor=upfuzz_cassandra:apache-cassandra-2.1.0_apache-cassandra-3.0.17)
+docker rm -f $(docker ps -a -q -f ancestor=upfuzz_cassandra:apache-cassandra-3.11.14_apache-cassandra-4.1.0_trunk)
 
 docker network prune -f
+docker container prune -f
