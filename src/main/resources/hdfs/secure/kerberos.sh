814270055f6e354cf9d46a6661dfb6bf1d39176d
Ke Han <kehan5800@gmail.com>
2022-12-15 12:52:36 -0600

    kerberos single node setup

---
+++ src/main/resources/hdfs/secure/kerberos.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# echo /etc/hosts, node0 will always be the KDC server
+
+# This only run on Node0
+
+
+IP=$(hostname --ip-address | cut -f 1 -d ' ')
+IP_MASK=$(echo $IP | cut -d "." -f -3)
+KDC_SERVER=$IP_MASK.2
+if [[ -z $(grep -F "kdc_server" "/etc/hosts") ]];
+then
+        echo "$KDC_SERVER    kdc_server" >> /etc/hosts
+fi
+
+
+IP_MASK=$(echo $IP | cut -d "." -f -3)
+HDFS_NAMENODE=$IP_MASK.2
+HDFS_SECONDARY_NAMENODE=$IP_MASK.3
+HDFS_DATANODE1=$IP_MASK.4
+HDFS_DATANODE2=$IP_MASK.5
+
+if [[ "$IP" == "$KDC_SERVER" ]];
+then
+        { echo 'password'; echo 'password'; } | krb5_newrealm
+        { echo 'password'; echo 'password'; } | kadmin.local addprinc ubuntu
+        { echo 'password'; echo 'password'; } | kadmin.local addprinc ubuntu/admin
+
+        # restart kdc
+        service krb5-admin-server restart
+        service krb5-kdc restart
+
+        kadmin.local addprinc -randkey hdfs/master
+        kadmin.local ktadd -k /master.keytab hdfs/master
+elif [[ "$IP" == "$HDFS_SECONDARY_NAMENODE" ]];
+then
+        { echo 'password';} | kadmin -p ubuntu/admin addprinc -randkey hdfs/secondarynn
+        { echo 'password';} | kadmin -p ubuntu/admin ktadd -k /secondarynn.keytab hdfs/secondarynn
+elif [[ "$IP" == "$HDFS_DATANODE1" ]];
+then
+        { echo 'password';} | kadmin -p ubuntu/admin addprinc -randkey hdfs/datanode1
+        { echo 'password';} | kadmin -p ubuntu/admin ktadd -k /datanode1.keytab hdfs/datanode1
+elif [[ "$IP" == "$HDFS_DATANODE2" ]];
+then
+        { echo 'password';} | kadmin -p ubuntu/admin addprinc -randkey hdfs/datanode2
+        { echo 'password';} | kadmin -p ubuntu/admin ktadd -k /datanode2.keytab hdfs/datanode2
+fi
+
