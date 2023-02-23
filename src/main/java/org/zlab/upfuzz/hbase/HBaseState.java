4c33ab65b50682b4e692f240be78130d6b7f5318
whilebug <whilebug@gmail.com>
2023-02-23 22:02:01 +0800

    HBase state init

--- src/main/java/org/zlab/upfuzz/hbase/HBaseState.java
+++ src/main/java/org/zlab/upfuzz/hbase/HBaseState.java
@@ -1,12 +1,55 @@
 package org.zlab.upfuzz.hbase;
 
+import org.zlab.upfuzz.Parameter;
 import org.zlab.upfuzz.State;
+import org.zlab.upfuzz.utils.Utilities;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class HBaseState extends State {
 
-    public HBaseState() {
+    public Map<String, Map<String, HBaseTable>> keyspace2tables = new HashMap<>();
+    public Map<String, Set<String>> keyspace2UDTs = new HashMap<>();
+
+    public void addTable(String keyspaceName, String tableName,
+                         HBaseTable table) {
+        keyspace2tables.get(keyspaceName).put(tableName, table);
     }
 
-    public void clearState() {
+    public void addKeyspace(String keyspaceName) {
+        if (!keyspace2tables.containsKey(keyspaceName)) {
+            keyspace2tables.put(keyspaceName, new HashMap<>());
+        }
+        if (!keyspace2UDTs.containsKey(keyspaceName)) {
+            keyspace2UDTs.put(keyspaceName, new HashSet<>());
+        }
     }
+
+    public Set<Parameter> getKeyspaces() {
+        return Utilities.strings2Parameters(keyspace2tables.keySet());
+    }
+
+    public Set<Parameter> getTablesInKeyspace(String keyspaceName) {
+        return Utilities
+                .strings2Parameters(keyspace2tables.get(keyspaceName).keySet());
+    }
+
+    public HBaseTable getTable(String keyspaceName, String tableName) {
+        return keyspace2tables.get(keyspaceName).get(tableName);
+    }
+
+    @Override
+    public void clearState() {
+        keyspace2tables.clear();
+        keyspace2UDTs.clear();
+    }
+
+    //public HBaseState() {
+    //}
+
+    //public void clearState() {
+    //}
 }
---
+++ src/main/java/org/zlab/upfuzz/hbase/HBaseTable.java
@@ -0,0 +1,38 @@
+package org.zlab.upfuzz.hbase;
+
+import java.io.Serializable;
+import java.util.*;
+import org.apache.commons.lang3.SerializationUtils;
+import org.zlab.upfuzz.Parameter;
+import org.zlab.upfuzz.ParameterType;
+import org.zlab.upfuzz.utils.Pair;
+
+public class HBaseTable implements Serializable {
+    public String name;
+    public List<Parameter> colName2Type;
+    public List<Parameter> primaryColName2Type;
+    // Doesn't support composite key now
+
+    public Set<String> indexes;
+
+    public HBaseTable(Parameter name, Parameter colName2Type,
+                          Parameter primaryColName2Type) {
+        this.name = (String) name.getValue();
+        if (colName2Type != null) {
+            this.colName2Type = new LinkedList<>();
+            for (Parameter col : (List<Parameter>) colName2Type.getValue()) {
+                this.colName2Type.add(SerializationUtils.clone(col));
+            }
+        }
+        if (primaryColName2Type != null) {
+            this.primaryColName2Type = new LinkedList<>();
+            for (Parameter primaryCol : (List<Parameter>) primaryColName2Type
+                    .getValue()) {
+                this.primaryColName2Type.add(
+                        SerializationUtils.clone(primaryCol));
+            }
+        }
+
+        indexes = new HashSet<>();
+    }
+}
