ea8107b24ca0daa4db03aa7fca393b0a63b128a4
whilebug <whilebug@gmail.com>
2023-02-28 21:35:55 +0800

    DROP command init

--- src/main/java/org/zlab/upfuzz/hbase/DataDefinitionCommands/DROP.java
+++ src/main/java/org/zlab/upfuzz/hbase/DataDefinitionCommands/DROP.java
@@ -1,4 +1,41 @@
 package org.zlab.upfuzz.hbase.DataDefinitionCommands;
 
-public class DROP {
+import org.zlab.upfuzz.Parameter;
+import org.zlab.upfuzz.ParameterType;
+import org.zlab.upfuzz.State;
+import org.zlab.upfuzz.hbase.HBaseCommand;
+import org.zlab.upfuzz.hbase.HBaseState;
+import org.zlab.upfuzz.utils.CONSTANTSTRINGType;
+
+public class DROP extends HBaseCommand {
+    public DROP(HBaseState state) {
+        Parameter keyspaceName = chooseKeyspace(state, this, null);
+        this.params.add(keyspaceName); // 0
+
+        Parameter TableName = chooseTable(state, this, null);
+        this.params.add(TableName); // 1
+
+        ParameterType.ConcreteType IF_EXISTType = new ParameterType.OptionalType(
+                new CONSTANTSTRINGType("IF EXISTS"), null // TODO: Make a
+                // pure
+                // CONSTANTType
+        );
+        Parameter IF_EXIST = IF_EXISTType
+                .generateRandomParameter(state, this);
+        params.add(IF_EXIST); // 2
+    }
+
+    @Override
+    public String constructCommandString() {
+        return "DROP " + params.get(2) +
+                " " + this.params.get(0) + "."
+                + this.params.get(1).toString() + ";";
+    }
+
+    @Override
+    public void updateState(State state) {
+        ((HBaseState) state).keyspace2tables
+                .get(params.get(0).toString())
+                .remove(params.get(1).toString());
+    }
 }
--- src/main/java/org/zlab/upfuzz/hbase/DataManipulationCommands/PUT.java
+++ src/main/java/org/zlab/upfuzz/hbase/DataManipulationCommands/PUT.java
@@ -1,4 +1,78 @@
 package org.zlab.upfuzz.hbase.DataManipulationCommands;
 
-public class PUT {
+import org.zlab.upfuzz.Parameter;
+import org.zlab.upfuzz.ParameterType;
+import org.zlab.upfuzz.State;
+import org.zlab.upfuzz.hbase.HBaseCommand;
+import org.zlab.upfuzz.hbase.HBaseState;
+import org.zlab.upfuzz.utils.Pair;
+
+import java.util.Collection;
+
+public class PUT extends HBaseCommand {
+
+    /*
+    * init0: 'key space name'
+    * init1: 'table name'
+    * init2: 'columnClusterName'
+    * init3: 'columnName'
+    * init3: 'value'
+    * */
+    public PUT(HBaseState state, Object init0, Object init1, Object init2, Object init3, Object init4) {
+
+        // 0. get key space name
+        Parameter keyspaceName = chooseKeyspace(state, this, init0);
+        this.params.add(keyspaceName); // [0]
+
+        // 1. get table name
+        Parameter TableName = chooseTable(state, this, init1);
+        this.params.add(TableName); // [1]
+
+        // 2. get column cluster name
+        ParameterType.ConcreteType columnsClusterType = new ParameterType.SuperSetType(
+                new ParameterType.SubsetType(null,
+                        (s, c) -> ((HBaseState) s).getTable(
+                                c.params.get(0).toString(),
+                                c.params.get(1).toString()).colName2Type,
+                        null),
+                (s, c) -> ((HBaseState) s).getTable(
+                        c.params.get(0).toString(),
+                        c.params.get(1).toString()).primaryColName2Type,
+                null);
+        Parameter columnClusters = columnsClusterType
+                .generateRandomParameter(state, this, init2);
+        this.params.add(columnClusters); // [2]
+
+        // 3. get column cluster name
+        ParameterType.ConcreteType columnsType = new ParameterType.SuperSetType(
+                new ParameterType.SubsetType(null,
+                        (s, c) -> ((HBaseState) s).getTable(
+                                c.params.get(0).toString(),
+                                c.params.get(1).toString()).colName2Type,
+                        null),
+                (s, c) -> ((HBaseState) s).getTable(
+                        c.params.get(0).toString(),
+                        c.params.get(1).toString()).primaryColName2Type,
+                null);
+        Parameter columns = columnsType
+                .generateRandomParameter(state, this, init2);
+        this.params.add(columns); // [2]
+
+        ParameterType.ConcreteType insertValuesType = new ParameterType.Type2ValueType(
+                null, (s, c) -> (Collection) c.params.get(2).getValue(), // columns
+                p -> ((Pair) ((Parameter) p).value).right);
+        Parameter insertValues = insertValuesType
+                .generateRandomParameter(state, this, init3);
+        this.params.add(insertValues); // [3]
+    }
+
+    @Override
+    public String constructCommandString() {
+        return null;
+    }
+
+    @Override
+    public void updateState(State state) {
+
+    }
 }
