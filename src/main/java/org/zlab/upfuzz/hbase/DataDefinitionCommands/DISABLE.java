91532cc1e9ab4d27d5e07c0f0c564db86edba56a
whilebug <whilebug@gmail.com>
2023-03-04 21:11:47 +0800

    DISABLE Realize

--- src/main/java/org/zlab/upfuzz/hbase/DataDefinitionCommands/DISABLE.java
+++ src/main/java/org/zlab/upfuzz/hbase/DataDefinitionCommands/DISABLE.java
@@ -1,4 +1,26 @@
 package org.zlab.upfuzz.hbase.DataDefinitionCommands;
 
-public class DISABLE {
+import org.zlab.upfuzz.Parameter;
+import org.zlab.upfuzz.State;
+import org.zlab.upfuzz.hbase.HBaseCommand;
+import org.zlab.upfuzz.hbase.HBaseState;
+
+import static org.zlab.upfuzz.hbase.HBaseCommand.chooseTable;
+
+public class DISABLE extends HBaseCommand {
+    public DISABLE(HBaseState state) {
+        Parameter tableName = chooseTable(state, this, null);
+        this.params.add(tableName); // 0 tableName
+    }
+
+    @Override
+    public String constructCommandString() {
+        return "DISABLE " + "'" + params.get(0) + "'";
+    }
+
+    @Override
+    public void updateState(State state) {
+        //((HBaseState) state).table2families
+        //        .remove(params.get(0).toString());
+    }
 }
