465be2ec9f97d59907bafe83d32aee3c4fda3c83
whilebug <whilebug@gmail.com>
2023-03-06 22:57:51 +0800

    fix bug

--- src/main/java/org/zlab/upfuzz/hbase/DataManipulationCommands/GET.java
+++ src/main/java/org/zlab/upfuzz/hbase/DataManipulationCommands/GET.java
@@ -1,15 +1,22 @@
 package org.zlab.upfuzz.hbase.DataManipulationCommands;
 
+import org.zlab.upfuzz.Parameter;
 import org.zlab.upfuzz.State;
 import org.zlab.upfuzz.hbase.HBaseCommand;
 import org.zlab.upfuzz.hbase.HBaseState;
 
 public class GET extends HBaseCommand {
-    public GET(HBaseState state){}
+    public GET(HBaseState state){
+        Parameter tableName = chooseTable(state, this, null);
+        this.params.add(tableName); // [0] table name
+
+        Parameter rowKey = chooseRowKey(state, this, null);
+        this.params.add(rowKey); // [1] row key
+    }
 
     @Override
     public String constructCommandString() {
-        return "GET " + "'" + params.get(0) + "'";
+        return "GET " + "'" + params.get(0) + "' " + "'" + params.get(1) + "'";
     }
 
     @Override
