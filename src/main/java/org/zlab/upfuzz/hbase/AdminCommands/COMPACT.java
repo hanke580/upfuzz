f743948bab9bc18df83814d7c52f1e6f8d67f26a
whilebug <whilebug@gmail.com>
2023-03-08 10:20:38 +0800

    add split and compact

---
+++ src/main/java/org/zlab/upfuzz/hbase/AdminCommands/COMPACT.java
@@ -0,0 +1,22 @@
+package org.zlab.upfuzz.hbase.AdminCommands;
+
+import org.zlab.upfuzz.Parameter;
+import org.zlab.upfuzz.State;
+import org.zlab.upfuzz.hbase.HBaseCommand;
+import org.zlab.upfuzz.hbase.HBaseState;
+
+public class COMPACT extends HBaseCommand {
+    public COMPACT(HBaseState state) {
+        Parameter tableName = chooseTable(state, this, null);
+        this.params.add(tableName); // 0 tableName
+    }
+
+    @Override
+    public String constructCommandString() {
+        return "COMPACT " + "'" + params.get(0) + "'";
+    }
+
+    @Override
+    public void updateState(State state) {
+    }
+}
