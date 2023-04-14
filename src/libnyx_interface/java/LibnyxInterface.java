8437cc1f29c730c5f73990193a60b8d309a4d9e7
asacchetti11 <asacche@purdue.edu>
2023-04-13 21:42:09 -0400

    nyxlib java portion of interface written

---
+++ src/libnyx_interface/java/LibnyxInterface.java
@@ -0,0 +1,69 @@
+public class LibnyxInterface {
+    static {
+        System.loadLibrary("libnyx_middleman.so");
+    }
+    
+    //Note each char is 16 bit = 64 bits total
+    private char[] nyx_process = new char[4]; // 64 bit pointer stored in string, should only ever be accessed within c
+
+    private String sharedir;
+    private String workdir;
+    private int cpuID;
+    private final int inputBufferSize = 1024 * 1024;
+    private final boolean inputBufferWriteProtection = true;
+
+    public LibnyxInterface(String sharedir, String workdir, int cpuID) {
+        this.sharedir = sharedir;
+        this.workdir = workdir;
+        this.cpuID = cpuID;
+    }
+
+    public String getSharedir() {
+        return this.sharedir;
+    }
+
+    public String getWorkdir() {
+        return this.workdir;
+    }
+
+    public int getInputBufferSize() {
+        return this.inputBufferSize;
+    }
+
+    public void nyxNew() {
+        this.nyxNew(this.sharedir, this.workdir, this.cpuID, this.inputBufferSize, this.inputBufferWriteProtection);
+    }
+
+    // Calls nyx_new to create the new nyx-vm instance
+    // This launches a VM with your specified Snapshot starting point in the sharedir
+    // sets the nyx_process to be used for all other functions
+    private native void nyxNew(String sharedir, String workdir, int cpu_id, int input_buffer_size, boolean input_buffer_write_protection);
+
+
+    // Calls nyx_shutdown on the nyx-vm instance
+    // This kills the VM
+    public native void nyxShutdown();
+
+    // Calls nyx_exec on the nyx-vm
+    // Lets the nyx-vm start the fuzzing loop, doesnt return until after checkpoint is reverted.
+    public native void nyxExec();
+
+    // Calls nyx_set_afl_input on the nyx-vm
+    // Sets input to be read in the fuzzing loop
+    public native void setInput(String input);
+
+
+    // Test Driver
+    public static void main(String[] args) {
+        LibnyxInterface l = new LibnyxInterface(
+            "/home/alessandro/new_nyx_mode/ubuntu/nyx_share",
+            "/tmp/tmp100",
+            0
+            );
+        l.nyxNew();
+        l.setInput("TEST");
+        l.nyxExec();
+        l.nyxShutdown();
+    }
+
+}
