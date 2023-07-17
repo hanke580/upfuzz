bf948de9db8c536689ec534ce1e3d9cab67457b1
Ke Han <kehan5800@gmail.com>
2023-07-17 12:21:28 -0400

    disable certain commands for evaluation

--- src/main/java/org/zlab/upfuzz/cassandra/CassandraCommandPool.java
+++ src/main/java/org/zlab/upfuzz/cassandra/CassandraCommandPool.java
@@ -18,9 +18,9 @@ public class CassandraCommandPool extends CommandPool {
         commandClassList.add(
                 new AbstractMap.SimpleImmutableEntry<>(ALTER_ROLE.class,
                         5));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(ALTER_TABLE.class,
-                        2));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(ALTER_TABLE.class,
+        // 2));
         commandClassList.add(
                 new AbstractMap.SimpleImmutableEntry<>(ALTER_TABLE_ADD.class,
                         5));
@@ -30,17 +30,17 @@ public class CassandraCommandPool extends CommandPool {
         commandClassList.add(
                 new AbstractMap.SimpleImmutableEntry<>(ALTER_TABLE_RENAME.class,
                         5));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(ALTER_TABLE_TYPE.class,
-                        5));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(ALTER_TYPE.class,
-                        5));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(ALTER_USER.class,
-                        5));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(CREATE_INDEX.class, 1));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(ALTER_TABLE_TYPE.class,
+        // 5));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(ALTER_TYPE.class,
+        // 5));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(ALTER_USER.class,
+        // 5));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(CREATE_INDEX.class, 1));
         commandClassList.add(
                 new AbstractMap.SimpleImmutableEntry<>(CREATE_KEYSPACE.class,
                         1));
@@ -49,24 +49,24 @@ public class CassandraCommandPool extends CommandPool {
         commandClassList.add(
                 new AbstractMap.SimpleImmutableEntry<>(CREATE_TYPE.class, 1));
 
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(DELETE.class, 6));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(DROP_INDEX.class, 6));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(DROP_KEYSPACE.class, 2));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(DROP_TABLE.class, 2));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(DROP_TYPE.class, 2));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(DELETE.class, 6));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(DROP_INDEX.class, 6));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(DROP_KEYSPACE.class, 2));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(DROP_TABLE.class, 2));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(DROP_TYPE.class, 2));
         commandClassList.add(
                 new AbstractMap.SimpleImmutableEntry<>(INSERT.class, 8));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(TRUNCATE.class, 8));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(UPDATE.class, 8));
-        commandClassList.add(
-                new AbstractMap.SimpleImmutableEntry<>(USE.class, 5));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(TRUNCATE.class, 8));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(UPDATE.class, 8));
+        // commandClassList.add(
+        // new AbstractMap.SimpleImmutableEntry<>(USE.class, 5));
 
         createCommandClassList.add(new AbstractMap.SimpleImmutableEntry<>(
                 CREATE_KEYSPACE.class, 2));
