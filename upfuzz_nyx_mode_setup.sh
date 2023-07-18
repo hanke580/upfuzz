52c6aef3bbbd3f35d567fc771184f2586c3ad9be
bPapan <bbasakpapan@gmail.com>
2023-07-18 16:17:46 -0400

    added command for resolving keyboard issues in Nyx

--- upfuzz_nyx_mode_setup.sh
+++ upfuzz_nyx_mode_setup.sh
@@ -5,3 +5,4 @@ mkdir $UPFUZZ_DIR/nyx_mode/ubuntu
 cd $UPFUZZ_DIR/nyx_mode/ubuntu
 ../packer/qemu_tool.sh create_image ubuntu.img $((1024*30))
 wget https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
+sed -i.bak 's/-k de//g' ../packer/qemu_tool.sh
