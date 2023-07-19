80eacde58f6c603fc674e647ebb8ee32eb999802
bPapan <bbasakpapan@gmail.com>
2023-07-19 15:18:12 -0400

    Script for minimal setup of Cassandra in Nyx VM

---
+++ minimal_setup_cassandra_nyx.sh
@@ -0,0 +1,64 @@
+##### Install Dependencies
+
+# jdk
+sudo apt-get install openjdk-11-jdk openjdk-8-jdk
+
+# docker
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+### Setup ssh for cloning upfuzz git repository
+
+cd ~
+ssh-keygen -t ed25519 -C "kehan5800@gmail.com"
+cat ~/.ssh/id_ed25519.pub
+
+git clone git@github.com:zlab-purdue/upfuzz.git
+
+### Complete the minimal setup for cassandra
+
+cd upfuzz
+export UPFUZZ_DIR=$PWD
+export ORI_VERSION=3.11.15
+export UP_VERSION=4.1.2
+
+mkdir -p "$UPFUZZ_DIR"/prebuild/cassandra
+cd prebuild/cassandra
+wget https://archive.apache.org/dist/cassandra/"$ORI_VERSION"/apache-cassandra-"$ORI_VERSION"-bin.tar.gz ; tar -xzvf apache-cassandra-"$ORI_VERSION"-bin.tar.gz
+wget https://archive.apache.org/dist/cassandra/"$UP_VERSION"/apache-cassandra-"$UP_VERSION"-bin.tar.gz ; tar -xzvf apache-cassandra-"$UP_VERSION"-bin.tar.gz
+
+sed -i 's/num_tokens: 16/num_tokens: 256/' apache-cassandra-"$UP_VERSION"/conf/cassandra.yaml
+
+cd ${UPFUZZ_DIR}
+cp src/main/resources/cqlsh_daemon2.py prebuild/cassandra/apache-cassandra-"$ORI_VERSION"/bin/cqlsh_daemon.py
+cp src/main/resources/cqlsh_daemon3_4.0.5_4.1.0.py  prebuild/cassandra/apache-cassandra-"$UP_VERSION"/bin/cqlsh_daemon.py
+
+cd src/main/resources/cassandra/normal/compile-src/
+docker build . -t upfuzz_cassandra:apache-cassandra-"$ORI_VERSION"_apache-cassandra-"$UP_VERSION"
+
+cd ${UPFUZZ_DIR}
+./gradlew copyDependencies
+./gradlew :spotlessApply build
+
+### Create load.sh file and make it executable
+
+cd ~
+echo "#\!/bin/bash
+sudo chmod 666 /var/run/docker.sock
+chmod 777 ./loader # or chmod +x ./loader
+sudo ./loader" > load.sh
+
+chmod +x load.sh
+
+sudo shutdown now
