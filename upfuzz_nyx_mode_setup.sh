9b6300ea9ad462ba6bc0c901f0954151d7144574
bPapan <bbasakpapan@gmail.com>
2023-07-19 15:15:25 -0400

    $PWD as parameter in upfuzz_nyx_mode_setup script

--- upfuzz_nyx_mode_setup.sh
+++ upfuzz_nyx_mode_setup.sh
@@ -1,4 +1,4 @@
-export UPFUZZ_DIR=$PWD
+UPFUZZ_DIR="$1"
 cd $UPFUZZ_DIR/nyx_mode 
 ./setup_nyx_mode.sh
 mkdir $UPFUZZ_DIR/nyx_mode/ubuntu
@@ -6,3 +6,4 @@ cd $UPFUZZ_DIR/nyx_mode/ubuntu
 ../packer/qemu_tool.sh create_image ubuntu.img $((1024*30))
 wget https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-live-server-amd64.iso
 sed -i.bak 's/-k de//g' ../packer/qemu_tool.sh
+../packer/qemu_tool.sh install ubuntu.img ubuntu-22.04.2-live-server-amd64.iso
