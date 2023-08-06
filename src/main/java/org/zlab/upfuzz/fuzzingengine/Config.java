a6e09917ddd2a5a7c679f78123e5ba7806209a09
Ke Han <kehan5800@gmail.com>
2023-08-06 13:24:09 -0400

    update ignore ratio from 80% to 20%

--- src/main/java/org/zlab/upfuzz/fuzzingengine/Config.java
+++ src/main/java/org/zlab/upfuzz/fuzzingengine/Config.java
@@ -122,8 +122,8 @@ public class Config {
 
         // ------------Likely Inv-------------
         public boolean useLikelyInv = false;
-        // if an invariant is broken for 80% times, ignore it
-        public double ignoreInvRatio = 0.8;
+        // if an invariant is broken over 20% test cases, ignore it
+        public double ignoreInvRatio = 0.2;
         public int runtimeMonitorPort = 62000;
         // if a inv is broken, we immediate skip it!
         public boolean skip = false;
