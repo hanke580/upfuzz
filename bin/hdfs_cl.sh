b6bcacd97dc40588d1ef1b0dffdacba196b20068
Ke Han <kehan5800@gmail.com>
2023-08-15 09:14:05 -0500

    update cl

--- bin/hdfs_cl.sh
+++ bin/hdfs_cl.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+OLD_VERSION=hadoop-2.10.2
+NEW_VERSION=hadoop-3.3.5
+
 pgrep -u $(id -u) -f config.json | xargs sudo kill -9
-docker rm -f $(docker ps -a -q -f ancestor=upfuzz_hdfs:hadoop-2.10.2_hadoop-3.3.5)
+docker rm -f $(docker ps -a -q -f ancestor=upfuzz_hdfs:${OLD_VERSION}_${NEW_VERSION})
 
 docker network prune -f
 docker container prune -f
