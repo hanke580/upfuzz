854a45ffef6cbe35b01ad7ccc7bba695581868bc
Ke Han <kehan5800@gmail.com>
2023-09-04 19:34:43 -0500

    update clean script

--- bin/hbase_cl.sh
+++ bin/hbase_cl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pgrep -f config.json | xargs sudo kill -9
-docker rm -f $(docker ps -a -q -f ancestor=upfuzz_hbase:hbase-2.4.15_hbase-2.5.2)
+docker rm -f $(docker ps -a -q -f ancestor=upfuzz_hbase:hbase-2.4.17_hbase-2.5.5)
 docker rm -f $(docker ps -a -q -f ancestor=upfuzz_hdfs:hadoop-2.10.2)
 
 docker network prune -f
