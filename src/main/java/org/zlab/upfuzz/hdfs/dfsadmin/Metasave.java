e19f1efb0a6a985921397612254d5a024a9e6666
Ke Han <kehan5800@gmail.com>
2024-04-13 15:00:29 -0400

    fix hdfs: metasave command

--- src/main/java/org/zlab/upfuzz/hdfs/dfsadmin/Metasave.java
+++ src/main/java/org/zlab/upfuzz/hdfs/dfsadmin/Metasave.java
@@ -1,11 +1,15 @@
 package org.zlab.upfuzz.hdfs.dfsadmin;
 
 import org.zlab.upfuzz.Parameter;
+import org.zlab.upfuzz.ParameterType;
 import org.zlab.upfuzz.State;
-import org.zlab.upfuzz.hdfs.HDFSParameterType.HDFSFilePathType;
 import org.zlab.upfuzz.hdfs.HdfsState;
 import org.zlab.upfuzz.utils.CONSTANTSTRINGType;
+import org.zlab.upfuzz.utils.STRINGType;
 
+/**
+ * Store metadata in a file under log dir in local FS system
+ */
 public class Metasave extends Dfsadmin {
 
     public Metasave(HdfsState state) {
@@ -14,11 +18,28 @@ public class Metasave extends Dfsadmin {
         Parameter cmd = new CONSTANTSTRINGType("-metasave")
                 .generateRandomParameter(null,
                         null);
-        Parameter file = new HDFSFilePathType()
-                .generateRandomParameter(state, null);
+        // filename: should be a random string...
+        Parameter file = new ParameterType.NotEmpty(
+                new STRINGType(20)).generateRandomParameter(state, null);
 
         params.add(cmd);
         params.add(file);
+    }
+
+    @Override
+    public String constructCommandStringWithDirSeparation(String type) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(type).append(" ");
+        int i = 0;
+        while (i < params.size() - 1) {
+            if (!params.get(i).toString().isEmpty())
+                sb.append(params.get(i)).append(" ");
+            i++;
+        }
+        // remove "/" from subdir: use string replace
+        String subdirWithoutSlash = this.subdir.replace("/", "");
+        sb.append(subdirWithoutSlash).append("_" + params.get(i));
+        return sb.toString();
     }
 
     @Override
