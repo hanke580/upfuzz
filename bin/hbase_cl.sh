c7ddd8f9d4b19b7e63c9db8e0d40033cbc9fbe3b
bPapan <bbasakpapan@gmail.com>
2024-04-17 05:35:38 -0400

    hbase configInfo files added

--- bin/hbase_cl.sh
+++ bin/hbase_cl.sh
@@ -6,6 +6,7 @@ pgrep -f config.json | xargs sudo kill -9
 pgrep --euid $USER qemu | xargs kill -9 # kill all lurking qemu instances
 docker rm -f $(docker ps -a -q -f ancestor=upfuzz_hbase:hbase-${OLD_VERSION}_hbase-${NEW_VERSION})
 docker rm -f $(docker ps -a -q -f ancestor=upfuzz_hdfs:hadoop-${OLD_VERSION})
+docker rm -f $(docker ps -a -q -f ancestor=upfuzz_hdfs:hadoop-2.10.2)
 
 docker network prune -f
 docker container prune -f
