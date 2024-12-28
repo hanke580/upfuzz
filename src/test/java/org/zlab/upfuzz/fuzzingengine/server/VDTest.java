d45cd3c173f07c1ab82f5f199b5ef40e8d542e0d
Ke Han <kehan5800@gmail.com>
2024-12-27 22:17:49 -0500

    update test

--- src/test/java/org/zlab/upfuzz/fuzzingengine/server/VDTest.java
+++ src/test/java/org/zlab/upfuzz/fuzzingengine/server/VDTest.java
@@ -12,39 +12,66 @@ import java.util.Set;
 
 public class VDTest {
 
-    public static void diff1(Map<String, Set<String>> modifiedFields1,
-            Map<String, Set<String>> modifiedFields2, boolean print) {
-        // print fields that only exist in modifiedFields1
+    public static int print(Map<String, Set<String>> classInfo) {
+        int count = 0;
+        for (Map.Entry<String, Set<String>> entry : classInfo.entrySet()) {
+            String className = entry.getKey();
+            Set<String> fields = entry.getValue();
+            for (String field : fields) {
+                System.out.println(className + "." + field);
+                count++;
+            }
+        }
+        return count;
+    }
 
-        // Diff between modifiedFields and modifiedFormatFields
-        // Only print the ones that exists in modifiedFormatFields but not in
-        // modifiedFields
+    public static Map<String, Map<String, String>> replaceDollarWithDot(
+            Map<String, Map<String, String>> classInfo) {
+        // Class => {fieldname, fieldtype}, only replace $ for classname
+        Map<String, Map<String, String>> newClassInfo = new HashMap<>();
+        for (Map.Entry<String, Map<String, String>> entry : classInfo
+                .entrySet()) {
+            String className = entry.getKey();
+            // deep copy fields
+            Map<String, String> fields = new HashMap<>();
+            for (Map.Entry<String, String> fieldEntry : entry.getValue()
+                    .entrySet()) {
+                String fieldName = fieldEntry.getKey();
+                String fieldType = fieldEntry.getValue();
+                fields.put(fieldName, fieldType);
+            }
+            newClassInfo.put(className.replace("$", "."), fields);
+        }
+        return newClassInfo;
+    }
+
+    public static Map<String, Set<String>> diff1(
+            Map<String, Set<String>> modifiedFields1,
+            Map<String, Set<String>> modifiedFields2) {
+        Map<String, Set<String>> modifiedFormatFields = new HashMap<>();
+        // Extract fields that only exist in modifiedFields1
         for (Map.Entry<String, Set<String>> entry : modifiedFields1
                 .entrySet()) {
             String className = entry.getKey();
             Set<String> fields = entry.getValue();
             if (!modifiedFields2.containsKey(className)) {
-                // print all
-                if (print) {
-                    for (String field : fields) {
-                        System.out.println(className + "." + field);
-                    }
-                }
+                modifiedFormatFields.computeIfAbsent(className,
+                        k -> new java.util.HashSet<>()).addAll(fields);
                 continue;
             }
             for (String field : fields) {
                 if (!modifiedFields2.get(className).contains(field)) {
-                    if (print)
-                        System.out.println(className + "." + field);
+                    modifiedFormatFields.computeIfAbsent(className,
+                            k -> new java.util.HashSet<>()).add(field);
                 }
             }
         }
+        return modifiedFormatFields;
     }
 
     public static Map<String, Set<String>> diff2(
             Map<String, Map<String, String>> classInfo1,
-            Map<String, Map<String, String>> matchableClassInfo,
-            boolean print) {
+            Map<String, Map<String, String>> matchableClassInfo) {
         // print all non-matchable references
         Map<String, Set<String>> modifiedFormatFields = new HashMap<>();
         for (Map.Entry<String, Map<String, String>> entry : classInfo1
@@ -57,8 +84,6 @@ public class VDTest {
                                 .containsKey(fieldName)) {
                     continue;
                 }
-                if (print)
-                    System.out.println(className + "." + fieldName);
                 modifiedFormatFields.computeIfAbsent(className,
                         k -> new java.util.HashSet<>()).add(fieldName);
             }
@@ -67,7 +92,7 @@ public class VDTest {
     }
 
     // @Test
-    public void printNonMatchableRef() {
+    public void run() {
         // For Debug
         new Config();
 
@@ -90,6 +115,10 @@ public class VDTest {
                         upFormatInfoFolder.resolve(
                                 Config.getConf().baseClassInfoFileName));
 
+        // Replace all $ with .
+        oriClassInfo = replaceDollarWithDot(oriClassInfo);
+        upClassInfo = replaceDollarWithDot(upClassInfo);
+
         Map<String, Map<String, String>> matchableClassInfo = Utilities
                 .computeMF(oriClassInfo, upClassInfo);
 
@@ -108,15 +137,31 @@ public class VDTest {
         }
 
         boolean printNonMatchable = true;
+        // print all non-matchable references
+        Map<String, Set<String>> modifiedFormatFields = diff2(oriClassInfo,
+                matchableClassInfo);
+        // print it
         if (printNonMatchable) {
-            // print all non-matchable references
-            Map<String, Set<String>> modifiedFormatFields = diff2(oriClassInfo,
-                    matchableClassInfo, false);
-            // Load modified fields...
-            Map<String, Set<String>> modifiedFields = Utilities
-                    .loadStringMapFromFile(
-                            configInfoFolder.resolve("modifiedFields.json"));
-            diff1(modifiedFormatFields, modifiedFields, false);
+            System.out.println("Non-Matchable References:");
+            int count = print(modifiedFormatFields);
+            System.out.println("Total: " + count);
         }
+
+        System.out.println();
+
+        boolean printDiff = true;
+        // Diff between static analysis and direct comparison
+        Map<String, Set<String>> modifiedFields = Utilities
+                .loadStringMapFromFile(
+                        configInfoFolder.resolve("modifiedFields.json"));
+        Map<String, Set<String>> diffFields = diff1(modifiedFormatFields,
+                modifiedFields);
+        if (printDiff) {
+            System.out.println("Diff:");
+            int count = print(diffFields);
+            System.out.println("Total: " + count);
+        }
+
+        //
     }
 }
